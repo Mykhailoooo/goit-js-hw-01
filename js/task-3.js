eb85893b42ae068492decee6722e95824c528b54
Mykhailoooo <fendo1335@gmail.com>
2024-04-17 11:57:21 +0300

    hw1.3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,9 @@
-function getElementWidth(content, padding, border) {return content + padding * 2 + border * 2 + 'px'};
-let content = 50;
-let padding = 10;
-let border = 5;
-
-console.log(getElementWidth(50, 8, 4)); 
-console.log(getElementWidth(60, 12, 8.5));
-console.log(getElementWidth(200, 0, 0));
+function getElementWidth(content, padding, border) {
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    return contentWidth + paddingWidth * 2 + borderWidth * 2; 
+};
+    console.log(getElementWidth("50px", "8px", "4px"));
+    console.log(getElementWidth("60px", "12px", "8.5px"));
+    console.log(getElementWidth("200px", "0px", "0px"));
